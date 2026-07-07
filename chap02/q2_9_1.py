a9260d057e6055cae884d99be6e65adabbda6d4e
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-07 02:22:57 +0000

    2-9終了

---
+++ chap02/q2_9_1.py
@@ -0,0 +1,7 @@
+print('apple' in ['pinapple','orange','banana'])
+
+print('apple'in'pineapple')
+"""実行結果
+False
+True
+"""
---
+++ chap02/q2_9_2.py
@@ -0,0 +1,11 @@
+x=True
+y=False
+z=None
+print(x and z is None)
+print(not x or not y)
+print(x and not y and z is None)
+"""実行結果
+True
+True
+True
+"""
