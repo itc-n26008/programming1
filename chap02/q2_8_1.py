b7a1f27ed65fc19760599ca963091ebc537c7884
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-07 01:54:14 +0000

    2-8終了

---
+++ chap02/q2_8_1.py
@@ -0,0 +1,11 @@
+my_list=[1,2,3,4,5,6,7,8,9,10]
+reverse_even=my_list[::-2]
+print(reverse_even)
+
+my_list=[1,2,3,4,5,6,7,8,9,10,11]
+reverse_even=my_list[-2::-2]
+print(reverse_even)
+"""実行結果
+[10, 8, 6, 4, 2]
+[10, 8, 6, 4, 2]
+"""
---
+++ chap02/q2_8_2.py
@@ -0,0 +1,12 @@
+my_list=['I','have','an','apple']
+my_list[2:4]=['a','pineapple']
+print(my_list)
+
+my_list=['I','have','an','apple']
+my_list[2:]=['a','pineapple']
+print(my_list)
+
+"""実行結果
+['I', 'have', 'a', 'pineapple']
+['I', 'have', 'a', 'pineapple']
+"""
