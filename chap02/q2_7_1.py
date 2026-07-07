919c18957ba18d8afe9d4ad150a5082c6d4d9136
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-07 01:28:00 +0000

    2-7終了

---
+++ chap02/q2_7_1.py
@@ -0,0 +1,9 @@
+my_list=[]
+my_list.append('orange')
+my_list.append('grape')
+my_list.append('banana')
+print(my_list)
+
+"""実行結果
+['orange', 'grape', 'banana']
+"""
---
+++ chap02/q2_7_2.py
@@ -0,0 +1,11 @@
+my_list=['orange','apple','grape','banana']
+my_list.sort()
+print(my_list)
+
+my_list=['orange','apple','grape','banana']
+my_list.sort(reverse=True)
+print(my_list)
+"""実行結果
+['apple', 'banana', 'grape', 'orange']
+['orange', 'grape', 'banana', 'apple']
+"""
