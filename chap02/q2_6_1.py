5b057148ae4ba0b6addd49f1ce9ba25450696735
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-07 01:25:32 +0000

    ２−６実行結果追加

--- chap02/q2_6_1.py
+++ chap02/q2_6_1.py
@@ -1,2 +1,9 @@
 my_str='abcdefg'
-print(my_str[-10])
+print(my_str[-10])#そんなものはないからエラーになる
+"""実行結果
+Traceback (most recent call last):
+  File "/vagrant/programming1/chap02/q2_6_1.py", line 2, in <module>
+    print(my_str[-10])
+          ~~~~~~^^^^^
+IndexError: string index out of range
+"""
--- chap02/q2_6_2.py
+++ chap02/q2_6_2.py
@@ -2,3 +2,6 @@ title='address'
 value='tokyo'
 string='{}:{}'
 print(string.format(title,value))
+"""実行結果
+address:tokyo
+"""
