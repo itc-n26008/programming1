82d55865cce7648f28bdb121027401f487491d1a
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-08 00:48:51 +0000

    2-12終了

---
+++ chap02/q2_12_1.py
@@ -0,0 +1,3 @@
+import math
+print(math.pow(2,3))
+"""実行結果 8.0"""
---
+++ chap02/q2_12_2.py
@@ -0,0 +1,8 @@
+print(random.randint(0,10))
+"""実行結果
+Traceback (most recent call last):
+  File "/vagrant/programming1/chap02/q2_12_2.py", line 1, in <module>
+    print(random.randint(0,10))
+          ^^^^^^
+NameError: name 'random' is not defined. Did you forget to import 'random'?
+"""
