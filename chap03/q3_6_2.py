c0d4aebd9d1b1cdc6ee6d43f18dfde505dc6f445
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-09 02:12:42 +0000

    3-6終了

--- chap03/q3_6_2.py
+++ chap03/q3_6_2.py
@@ -4,17 +4,17 @@ num4 =''.join(random.sample(numbers, k=4))
 while True:
     val= input()
     if val == num4:
-        print(正解です！)
+        print('正解です！')
         break
     if len(val) != 4:#文字数が４じゃなかったら
         print('数字４ついれなさいね')
         continue
     answer =''
     for i in range(4):#文字一つずつ確認
-        if num4[i]==val[i]:その１文字があってたら、
-            answer += num4[i]answer二追加
+        if num4[i]==val[i]:#その１文字があってたら、
+            answer += num4[i]#answer二追加
         else:
-            answer += 'X'あってなかったら、Xを追加
+            answer += 'X'#あってなかったら、Xを追加
     print('->'+ answer)
 """実行結果　    
 7777 途中から
