46787c76e1530fcdeeab3ec7697fdae029737007
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-14 00:57:46 +0000

    3-7終了

--- chap03/q3_7_2.py
+++ chap03/q3_7_2.py
@@ -1,6 +1,9 @@
 with open('number.txt','r') as f:
     sum=0
-    for data in f:
-        num = int(data)
-        sum += num
+    for data in f:#dataの中に一つづつ数を入れていく
+        num = int(data)#numはdataをint型にしたやつ
+        sum += num#取り出した整数をsumに足していく
     print(sum)
+"""実行結果 55
+テキストの中は、１から１０が書いてある
+"""
