0caf0f249c7f4deff96f3abf31b9203204c15345
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-28 02:53:37 +0000

    5-8 終了

--- chap05/q5_8_1.py
+++ chap05/q5_8_1.py
@@ -5,13 +5,14 @@ country_code={
         }
 
 def getstr_keyval(x):
-    if not isinstance(x,dict):
+    if not isinstance(x,dict):#isinstanceは型を調べる機能、dictは辞書、
+        #辞書じゃなかったらそのまま外に返す
         return x
 
     my_str = ''
-    for key, val in x.items():
+    for key, val in x.items():#セットで取り出す、items
         my_str +=(' '+ str(key) + ' ' + getstr_keyval(val))
-    return my_str
+    return my_str#作った文字列を返す
 
 for key1, val1 in country_code.items():
     print(key1, getstr_keyval(val1))
