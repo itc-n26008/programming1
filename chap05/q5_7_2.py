fe3d49aec9941d571500d05fda6c4a41145883be
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-28 01:20:13 +0000

    5-7了

--- chap05/q5_7_2.py
+++ chap05/q5_7_2.py
@@ -2,12 +2,30 @@
 
 data=[
         ['0001','Male','Yamada','Tarou',25,'Tokyo'],
-        ['0002','Male','Satou','Takahashi','27','Kanagawa'],
+        ['0002','Male','Satou','Takahashi',27,'Kanagawa'],
         ['0003','Female','Tanaka','Yuko',25,'Saitama'],
         ['0004','Male','Suzuki','Ichirou',35,'Hokkaido']
         ]
 print(data)
 
+#辞書変数生成
+member_information={}
+
+#表データをレコード毎に格納する
+for record in data:
+    key=record[0]
+    info=record[1:]
+    member_information[key]=info
+
+#結果を表示する
+print('number','information',sep='\t')
+for key, info in member_information.items():
+    print(key,info)
 '''実行結果
-[['0001', 'Male', 'Yamada', 'Tarou', 25, 'Tokyo'], ['0002', 'Male', 'Satou', 'Takahashi', '27', 'Kanagawa'], ['0003', 'Female', 'Tanaka', 'Yuko', 25, 'Saitama'], ['0004', 'Male', 'Suzuki', 'Ichirou', 35, 'Hokkaido']]
+[['0001', 'Male', 'Yamada', 'Tarou', 25, 'Tokyo'], ['0002', 'Male', 'Satou', 'Takahashi', 27, 'Kanagawa'], ['0003', 'Female', 'Tanaka', 'Yuko', 25, 'Saitama'], ['0004', 'Male', 'Suzuki', 'Ichirou', 35, 'Hokkaido']]
+number	information
+0001 ['Male', 'Yamada', 'Tarou', 25, 'Tokyo']
+0002 ['Male', 'Satou', 'Takahashi', 27, 'Kanagawa']
+0003 ['Female', 'Tanaka', 'Yuko', 25, 'Saitama']
+0004 ['Male', 'Suzuki', 'Ichirou', 35, 'Hokkaido']
 '''
