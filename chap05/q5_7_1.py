c7a4541a72f0e9b84561e30d121fa62fbc56c76e
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-28 01:05:14 +0000

    5-7了

---
+++ chap05/q5_7_1.py
@@ -0,0 +1,8 @@
+yamamoto_line={0: 'Tokyo', 1: 'Yuurakuchou', 2:'Shinbashi'}
+for v in yamamoto_line.values():
+    print(v)
+'''実行結果
+Tokyo
+Yuurakuchou
+Shinbashi
+'''
---
+++ chap05/q5_7_2.py
@@ -0,0 +1,13 @@
+#表データの作成
+
+data=[
+        ['0001','Male','Yamada','Tarou',25,'Tokyo'],
+        ['0002','Male','Satou','Takahashi','27','Kanagawa'],
+        ['0003','Female','Tanaka','Yuko',25,'Saitama'],
+        ['0004','Male','Suzuki','Ichirou',35,'Hokkaido']
+        ]
+print(data)
+
+'''実行結果
+[['0001', 'Male', 'Yamada', 'Tarou', 25, 'Tokyo'], ['0002', 'Male', 'Satou', 'Takahashi', '27', 'Kanagawa'], ['0003', 'Female', 'Tanaka', 'Yuko', 25, 'Saitama'], ['0004', 'Male', 'Suzuki', 'Ichirou', 35, 'Hokkaido']]
+'''
