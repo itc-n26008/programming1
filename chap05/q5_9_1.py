a6916bded2643ce44b1b5f9949c14f9fcbf22648
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-29 01:11:31 +0000

    5-9 終了

--- chap05/q5_9_1.py
+++ chap05/q5_9_1.py
@@ -2,3 +2,6 @@ mountain_in_japan ={'fuji': 3776,'kitadake': 3193, 'okuhodakadake': 3190, 'dummy
 mountain_in_japan_sorted = sorted(mountain_in_japan.items(),
                                   key=lambda x: x[1], reverse=True)
 print(mountain_in_japan_sorted)
+'''実行結果
+[('fuji', 3776), ('kitadake', 3193), ('okuhodakadake', 3190), ('dummy', 0)]
+'''
