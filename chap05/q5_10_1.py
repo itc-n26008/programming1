f279f8ae3d18ccd10dc8d9dc7b5c5fa9b1bbe755
itc-n26008 <n26008@std.it-college.ac.jp>
2026-07-29 02:19:47 +0000

    5-10 終了

---
+++ chap05/q5_10_1.py
@@ -0,0 +1,9 @@
+def is_number_of_positions_of_10(x):
+    return 10 <= x<20
+
+print(is_number_of_positions_of_10(12),
+is_number_of_positions_of_10(20),
+is_number_of_positions_of_10(9))
+'''実行結果
+True False False
+'''
