887baf8863d177f38ad38195d63a9113cb7d904a
itc-n26008 <n26008@std.it-college.ac.jp>
2026-08-19 10:12:00 +0900

    ７−１終了

---
+++ chap07/q7_1_3.py
@@ -0,0 +1,8 @@
+print(int('3.5'))
+
+'''出力結果
+Traceback (most recent call last):
+  File "/vagrant/programming1/chap07/q7_1_3.py", line 1, in <module>
+    print(int('3.5'))
+          ^^^^^^^^^^
+ValueError: invalid literal for int() with base 10: '3.5'''
---
+++ chap07/q7_1_4.py
@@ -0,0 +1,9 @@
+my_list = []
+print(my_list.insert(3))
+'''出力結果
+Traceback (most recent call last):
+  File "/vagrant/programming1/chap07/q7_1_4.py", line 2, in <module>
+    print(my_list.insert(3))
+          ^^^^^^^^^^^^^^^^^
+TypeError: insert expected 2 arguments, got 1
+'''
